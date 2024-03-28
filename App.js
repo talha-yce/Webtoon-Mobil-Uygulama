975fca2d79f49b11e4e0a0e2c9e4a9f6f439de15
Talha Yüce <yucetalha8290@gmail.com>
2024-03-28 14:53:38 +0300

    Bug düzeltmeleri yapıldı

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './src/component/LoginScreen';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import LoginScreen from './src/components/LoginScreen';
 export default function App() {
   return (
     <View style={styles.container}>
---
+++ src/components/Anasayfa.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, TextInput, Pressable, Text, Image } from 'react-native';
+
+const Anasayfa = () => {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+export default Anasayfa;
