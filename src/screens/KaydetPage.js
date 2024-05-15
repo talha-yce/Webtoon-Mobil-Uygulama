d440adab1563c432d4c3b1dd7ef736c226392b85
Talha <yucetalha8290@gmail.com>
2024-05-15 23:18:06 +0300

    Kaydet sayfası için arama özelliği eklendi

--- src/screens/KaydetPage.js
+++ src/screens/KaydetPage.js
@@ -1,49 +1,122 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet, TextInput, ScrollView } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet, TextInput, ScrollView,RefreshControl } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { getDocs, collection } from 'firebase/firestore';
+import { getDocs, collection, query,getDoc,doc } from 'firebase/firestore';
 import { db } from '../../firebaseConfig'; 
-import { useDispatch, useSelector } from 'react-redux';
-import { lightTheme, darkTheme,DarkToonTheme} from '../components/ThemaStil';
+import { useSelector } from 'react-redux';
+import { lightTheme, darkTheme, DarkToonTheme } from '../components/ThemaStil';
+import { getAuth } from "firebase/auth";
+
 const KaydetPage = () => {
   const navigation = useNavigation();
-  const [categories, setCategories] = useState([]);
   const [searchText, setSearchText] = useState('');
+  const [webtoonsData, setWebtoonsData] = useState([]);
   const theme = useSelector(state => state.user.theme);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
 
   useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const categoriesSnapshot = await getDocs(collection(db, 'recorded'));
-        const categoriesData = await Promise.all(categoriesSnapshot.docs.map(async doc => {
-          const webtoonsSnapshot = await getDocs(collection(db, `recorded/${doc.id}/webtoons`));
-          const webtoonsData = webtoonsSnapshot.docs.map(webtoonDoc => webtoonDoc.id);
-          return {
-            id: doc.id,
-            webtoons: webtoonsData
-          };
-        }));
-        
-        setCategories(categoriesData);
-      } catch (error) {
-        console.error('Kategorileri getirirken hata oluştu:', error);
-      }
-    };
-
-    fetchCategories();
+    fetchWebtoons();
   }, []);
 
-  const handleCategorySelect = (webtoon) => {
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await fetchWebtoons();
+    setRefreshing(false);
+  };
+  
+  const fetchWebtoons = async () => {
+    try {
+      
+      const user = getAuth().currentUser;
+      if (user) {
+        const userId = user.uid;
+      const userDoc = await getDoc(doc(db, 'users', userId));
+  
+      if (userDoc.exists()) {
+        // Kullanıcının kayıtlı webtoonları varsa
+        const savedWebtoons = userDoc.data().kaydet || [];
+  
+        // Kayıtlı webtoonları al
+        const webtoonsData = await Promise.all(savedWebtoons.map(async webtoonId => {
+          // Webtoon bilgilerini al
+          const webtoonDoc = await getDoc(doc(db, 'webtoonlar', webtoonId));
+          
+          if (webtoonDoc.exists()) {
+            const webtoonData = webtoonDoc.data();
+            return {
+              adı: webtoonId,
+              tur: webtoonData.tur || []
+            };
+          } else {
+            console.error(`Webtoon ID'si "${webtoonId}" olan webtoon bulunamadı.`);
+            return null;
+          }
+        }).filter(webtoon => webtoon !== null)); // Null olanları filtrele
+  
+        setWebtoonsData(webtoonsData);
+        setLoading(false);
+      } else {
+        console.error(`Kullanıcı ID'si "${userId}" olan kullanıcı bulunamadı veya belirtilen koleksiyon dökümanı yok.`);
+      }
+    }
+    else{console.log("Kullanıcı giriş yapmamaış")}
+  } catch (error) {
+      console.error('Webtoonları getirirken hata oluştu:', error);
+    }
+  };
+  
+  
+
+  const handleWebtoonSelect = (webtoon) => {
     console.log(`Seçilen webtoon: ${webtoon}`);
     navigation.navigate('WebtoonInfoPage', { webtoon: webtoon });
   };
 
-  const handleSearch = () => {
-    console.log('Arama yapıldı');
-    // Firestore'dan arama işlemleri buraya eklenebilir.
+  const handleSearch = async () => {
+    try {
+      if (searchText.trim() === '') {
+        fetchWebtoons();
+        return;
+      }
+  
+      const user = getAuth().currentUser;
+      if (user) {
+        const userId = user.uid;
+      const userDoc = await getDoc(doc(db, 'users', userId));
+  
+      if (userDoc.exists()) {
+        // Kullanıcının kayıtlı webtoonları varsa
+        const savedWebtoons = userDoc.data().kaydet || [];
+  
+        // Eğer aranan webtoon kullanıcının kayıtlı webtoonları arasında varsa
+        if (savedWebtoons.includes(searchText)) {
+          const webtoonDoc = await getDoc(doc(db, 'webtoonlar', searchText));
+          if (webtoonDoc.exists()) {
+            const webtoonData = webtoonDoc.data();
+  
+            const searchedWebtoons = [{
+              adı: searchText,
+              tur: webtoonData.tur || []
+            }];
+  
+            setWebtoonsData(searchedWebtoons);
+          } else {
+            setWebtoonsData([]);
+          }
+        } else {
+          // Kullanıcı kayıtlı webtoonları arasında aranan webtoon bulunmuyorsa
+          setWebtoonsData([]);
+        }
+      } else {
+        console.error(`Kullanıcı ID'si "${userId}" olan kullanıcı bulunamadı veya belirtilen koleksiyon dökümanı yok.`);
+      }}else{console.log("Kullanıcı giriş yapmamış")}
+    } catch (error) {
+      console.error('Arama yapılırken hata oluştu:', error);
+    }
   };
-
-
+  
+  
 
 
   return (
@@ -51,7 +124,6 @@ const KaydetPage = () => {
     ? DarkToonTheme.purpleStil.backgroundColor: theme === 'lightTheme'
       ? lightTheme.whiteStil.backgroundColor
       : darkTheme.darkStil.backgroundColor }]}>
-      {/* Header */}
       <View style={styles.header}>
         <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
           <Image source={require('../../assets/İmage/HomePage_images/settings.png')} style={styles.settingicon} />
@@ -69,6 +141,7 @@ const KaydetPage = () => {
           <Text style={styles.bildirimicon} />
         </TouchableOpacity>
       </View>
+
       <View style={styles.searchContainer}>
         <View style={styles.searchTextContainer}>
           <TextInput
@@ -90,27 +163,43 @@ const KaydetPage = () => {
         </View>
       </View>
 
-      <ScrollView style={styles.scrollView}>
-        {categories.map(category => (
-          <View key={category.id} style={styles.categoryContainer}>
-            <Text style={styles.categoryTitle}>{category.id}</Text>
-            <View style={styles.webtoonList}>
-              {category.webtoons.map(webtoon => (
-                <TouchableOpacity 
-                  key={webtoon} 
-                  style={styles.webtoonItem} 
-                  onPress={() => handleCategorySelect(webtoon)}
-                >
-                  <Text style={styles.webtoonText}>{webtoon}</Text>
-                </TouchableOpacity>
-              ))}
-            </View>
+      <ScrollView style={styles.scrollView} refreshControl={
+    <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+        {loading ? ( 
+          <Text style={styles.loadingText}>Veriler yükleniyor...</Text>
+        ) : (
+          webtoonsData.length > 0 ? (
+            webtoonsData.map(webtoon => (
+              <TouchableOpacity key={webtoon.adı} onPress={() => handleWebtoonSelect(webtoon.adı)}>
+                <View style={styles.webtoonContainer}>
+          <Text style={styles.webtoonTitle}>{webtoon.adı}</Text>
+          <View style={styles.turContainer}>
+            {webtoon.tur.slice(0, 4).map((tur, index) => (
+              <View key={index} style={styles.turBox}>
+                <Text style={styles.turText}>{tur}</Text>
+              </View>
+            ))}
+            {webtoon.tur.length > 4 && (
+              <View style={styles.moreTurBox}>
+                <Text style={styles.moreTurText}>+{webtoon.tur.length - 4} daha</Text>
+              </View>
+            )}
           </View>
-        ))}
+        </View>
+        </TouchableOpacity>
+            ))
+          ) : (
+            <View style={styles.webtoonContainer}>
+              <Text style={styles.webtoonTitle}>Aradığınız webtoon bulunamadı.</Text>
+            </View>
+          )
+        )}
       </ScrollView>
-      {/* alt navigaysyon bölümu*/}
+
+      
+      
       <View style={styles.bottomNav}>
-      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
           <Image source={require('../../assets/İmage/HomePage_images/home.png')} style={styles.navIcon} />
         </TouchableOpacity>
         <TouchableOpacity onPress={() => navigation.navigate('Kesfet')}>
@@ -125,12 +214,11 @@ const KaydetPage = () => {
       </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'purple',
     paddingTop: 20,
   },
   header: {
@@ -170,52 +258,6 @@ const styles = StyleSheet.create({
   bildirimicon: {
     width: 35,
     height: 35,
-  },
-  profileContent: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  profileHeaderText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  bottomNav: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 25,
-    paddingBottom: 15,
-    paddingTop: 10,
-  },
-  navIcon: {
-    width: 35,
-    height: 35,
-  },
-  categoryContainer: {
-    paddingHorizontal: 25,
-    marginTop: 10,
-  },
-  categoryTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 5,
-    color: 'black',
-  },
-  webtoonList: {
-    backgroundColor: 'purple',
-    borderRadius: 5,
-    padding: 10,
-  },
-  webtoonItem: {
-    paddingVertical: 5,
-  },
-  webtoonText: {
-    color: 'white',
-  },
-  scrollView: {
-    paddingHorizontal: 20,
-    backgroundColor: 'white',
   },
   searchContainer: {
     backgroundColor: 'white',
@@ -264,6 +306,87 @@ const styles = StyleSheet.create({
   filterIcon: {
     width: 30,
     height: 30,
+  },
+  categoryContainer: {
+    paddingHorizontal: 25,
+    marginTop: 10,
+  },
+  bottomNav: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 25,
+    paddingBottom: 15,
+    paddingTop: 10,
+  },
+  navIcon: {
+    width: 35,
+    height: 35,
+  },
+  categoryTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: 'black',
+  },
+  webtoonList: {
+    backgroundColor: 'purple',
+    borderRadius: 5,
+    padding: 10,
+  },
+  webtoonItem: {
+    paddingVertical: 5,
+  },
+  webtoonText: {
+    color: 'white',
+  },
+  scrollView: {
+    paddingHorizontal: 20,
+    backgroundColor: 'white',
+  },
+  webtoonContainer: {
+    marginBottom: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    borderRadius: 10,
+  },
+  webtoonTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  turContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  turBox: {
+    backgroundColor: 'purple',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+    marginBottom: 10,
+  },
+  turText: {
+    color: 'white',
+  },
+  moreTurBox: {
+    backgroundColor: 'gray',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+    marginBottom: 10,
+  },
+  moreTurText: {
+    color: 'white',
+  },
+  loadingText: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 16,
+    color: 'gray',
   },
 });
 
